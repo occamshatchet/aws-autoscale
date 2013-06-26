0065ea456bbd616b786f678266fdebeffb6142dc
Wil Wang <wil.wang@gmail.com>
2013-06-26 11:38:26 -0400

    retrieve instances from graphite return scaling decision

--- graphite-script.js
+++ graphite-script.js
@@ -1,73 +1,134 @@
 var http = require('http');
-
-var target = process.argv[2]
-    , from = process.argv[3]
-    , threshold = parseInt(process.argv[4])
-    , instances = parseInt(process.argv[5]);
-
-var  baseline_instances = 2;
-var url = '/render?format=json&target=' + target + '&from=' + from;
-//var url = 'http://stats/render?format=json&target=sensu.prod.rabbitmq.nva-a-rabbitmq-p01.queues.SwaptionValuationQueueRequestQueue.messages&from=-5min';
-
-var options = {
-    host: 'stats',
-    port: 80,
-    path: url,
-    method: 'GET'
+var config = {
+    instancesUrl : 'http://stats/render?format=json&target=sensu.aws.autoscale.lmm-prod.instance_count&from=-1min',
+    messagesUrl : 'http://stats/render?format=json&target=sensu.prod.rabbitmq.nva-a-rabbitmq-p01.queues.SwaptionValuationQueueRequestQueue.messages&from=-5min',
+    threshold : 20,
+    baseline_instances : 2
 };
 
-var req = http.request(options, function(res) {
-    var data = '';
-    
-    res.setEncoding('utf8');
-    res.on('data', function(chunk) {
-        data += chunk;
-    });
 
-    res.on('end', function() {
-        var jsonData = JSON.parse(data);
-        var points = jsonData[0].datapoints;
-        var sumPoints = 0;
-        var numPoints = points.length;
-        var count = 0;
+function getInstances(callback) {
+    var options = {
+        host: 'stats',
+        port: 80,
+        path: config.instancesUrl,
+        method: 'GET'
+    };
 
-        while(count < numPoints) {
-            sumPoints += points[count][0];
-            count++;
-        }
+    var req = http.request(options, function(res) {
+        var data = '';
+        
+        res.setEncoding('utf8');
+        res.on('data', function(chunk) {
+            data += chunk;
+        });
 
-        var avg = sumPoints/numPoints;
-        var scale_dir = "neutral";
+        res.on('end', function() {
+            if (res.statusCode === 200) {
+                var jsonData = JSON.parse(data);
+                var instances = jsonData[0].datapoints[0][0];
 
-        // just assuming on/off right now rather 
-        // than graduated scale up/down
-        if (avg > threshold) {
-            if (instances <= baseline_instances) {
-                scale_dir = "up";
-            } else { 
-                scale_dir = "neutral";
-            }
-        } else {
-            if (instances > baseline_instances) {
-                scale_dir = "down";
+                callback(null, instances);
             } else {
-                scale_dir = "neutral";
+                callback("Server returned " + res.statusCode + "\n" + instancesUrl, null);
             }
-        }
-
-        console.log({
-            target: target,
-            current_instances: instances,
-            baseline_instances: baseline_instances,
-            threshold: threshold,
-            average: avg,
-            result: scale_dir
         });
     });
-});
 
-req.on('error', function(e) {
-    console.log(e);
-});
+    req.on('error', function(e) {
+        callback(e, null);
+    });
 
-req.end();
+    req.end();
+}
+
+function getAvgMessageLoad(callback) {
+    var options = {
+        host: 'stats',
+        port: 80,
+        path: config.messagesUrl,
+        method: 'GET'
+    };
+
+    var req = http.request(options, function(res) {
+        var data = '';
+        
+        res.setEncoding('utf8');
+        res.on('data', function(chunk) {
+            data += chunk;
+        });
+
+        res.on('end', function() {
+            if (res.statusCode === 200) {
+                var jsonData = JSON.parse(data);
+                var points = jsonData[0].datapoints;
+                var sumPoints = 0;
+                var numPoints = points.length;
+                var count = 0;
+
+                while(count < numPoints) {
+                    sumPoints += points[count][0];
+                    count++;
+                }
+
+                var avg = sumPoints/numPoints;
+
+                callback(null, avg);
+            } else {
+                callback("Server returned " + res.statusCode + "\n" + messagesUrl, null);
+            }
+        });
+    });
+
+    req.on('error', function(e) {
+        callback(e, null);
+    });
+
+    req.end();    
+}
+
+function getScaleDirection() {
+    getInstances(function(err, instances) {
+        if (err) {
+            console.log({ code: -1, message: err });
+            process.exit(1);
+        }
+
+        getAvgMessageLoad(function(err, average) {
+            if (err) {
+                console.log({ code: -1, message: err });
+                process.exit(1);
+            }
+
+            // 100 == scale down
+            // 200 == neutral
+            // 300 == scale up
+            var scale_dir = 200;
+            var scale_msg = "NEUTRAL";
+
+            if (average > config.threshold) {
+                if (instances <= config.baseline_instances) {
+                    scale_dir = 300;
+                    scale_msg = "UP";
+                }
+            } else {
+                if (instances > config.baseline_instances) {
+                    scale_dir = 100;
+                    scale_msg = "DOWN";
+                }
+            }
+
+            console.log({ 
+                exit_code : scale_dir + '[' + scale_msg + ']',
+                averageMsgLoad : average,
+                threshold: config.threshold,
+                runningInstances : instances,
+                baseline_instances : config.baseline_instances
+            });
+            process.exit(scale_dir);
+        });
+    });
+}
+
+
+getScaleDirection();
